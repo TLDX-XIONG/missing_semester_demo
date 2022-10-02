8ac87a6eabaad042ea065e1f586921a0ced1d712
TLDX <2029949969@qq.com>
2022-10-02 12:51:10 +0800

    Add dog function

--- animal.py
+++ animal.py
@@ -3,8 +3,14 @@ import sys
 def default():
     print('hello')
 
+def dog():
+    print('Woew')
+
 def main():
-    default()
+    if sys.argv[1] == 'dog':
+    	dog()
+    else:
+    	default()
 
 if __name__ == '__main__':
     main()
